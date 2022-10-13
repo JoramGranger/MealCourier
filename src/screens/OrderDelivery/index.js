8f8d36999e7c677261e97eb8548d413a4c752afc
JORAMGRANGER <joramgranger@outlook.com>
2022-10-13 20:23:07 +0300

    commit 2

--- src/screens/OrderDelivery/index.js
+++ src/screens/OrderDelivery/index.js
@@ -1,8 +1,9 @@
 import { View, Text } from 'react-native';
 import { useRef, useState,useMemo } from "react";
 import BottomSheet from '@gorhom/bottom-sheet';
-import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome5, Fontisto } from '@expo/vector-icons';
 import orders from '../../../assets/data/orders.json';
+import styles from './styles';
 
 const order = orders[0];
 
@@ -12,30 +13,40 @@ const OrderDelivery = () => {
     const snapPoints = useMemo(() => ["12%", "95%"], [])
 
     return ( 
-        <View style={{backgroundColor: 'lightgrey', flex: 1}}>
+        <View style={styles.container}>
             <BottomSheet 
             ref={bottomSheetRef} 
             snapPoints={snapPoints}
-            handleIndicatorStyle={{backgroundColor: "black", width: 100}}
+            handleIndicatorStyle={styles.handleIndicator}
             >
-                <View style={{ marginTop: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-                    <Text style={{fontSize: 25, letterSpacing: 1}}>14 min</Text>
+                <View style={styles.handleIndicatorContainer}>
+                    <Text style={styles.routeDetailsText}>14 min</Text>
                     <FontAwesome5 name="shopping-bag" size={30} color="#3fc060" style={{
                         marginHorizontal: 10
                     }}/>
-                    <Text style={{fontSize: 25, letterSpacing: 1}}>5 km</Text>
+                    <Text style={styles.routeDetailsText}>5 km</Text>
                 </View>
 
-                <View style={{paddingHorizontal: 20}}>
-                    <Text style={{fontSize: 25, letterSpacing: 1, paddingVertical: 20}}>{order.Restaurant.name}</Text> 
-                    <Text style={{fontSize: 20, color: 'grey', fontWeight: '500', letterSpacing: 1}}>{order.Restaurant.address}</Text> 
-                    <Text style={{fontSize: 20, color: 'grey', fontWeight: '500', letterSpacing: 1}}>{order.User.address}</Text>
-                    <View style={{borderTopWidth: 1, borderColor: 'lightgrey'}}>
-                        <Text>Onion Rings x1</Text> 
-                        <Text>Chicken Drum x1</Text> 
-                        <Text>Chapat x3</Text> 
-                    <Text>Coca Cola coke x2</Text> 
+                <View style={styles.orderDeilveryDetailsContainer}>
+                    <Text style={styles.restaurantName}>{order.Restaurant.name}</Text> 
+                    <View style={styles.addressContainer}>
+                        <Fontisto name="shopping-store" size={22} color="grey" />
+                        <Text style={styles.addressText}>{order.Restaurant.address}</Text>                         
+                    </View>
+                    <View style={styles.addressContainer}>
+                        <FontAwesome5 name="map-marker-alt" size={30} color="grey" />
+                        <Text style={styles.addressText}>{order.User.address}</Text>                                               
+                    </View>
+                    
+                    <View style={styles.orderDetailsContainer}>
+                        <Text style={styles.orderItemText}>Onion Rings x1</Text> 
+                        <Text style={styles.orderItemText}>Chicken Drum x1</Text>
+                        <Text style={styles.orderItemText}>Chapat x3</Text> 
+                        <Text style={styles.orderItemText}>Coca Cola coke x2</Text> 
                 </View>
+                </View>
+                <View style={{backgroundColor: "#3fc060", marginTop: 'auto', marginVertical: 30, marginHorizontal: 10, borderRadius: 10}}>
+                    <Text style={{color: 'white', paddingVertical: 15, fontSize: 25, fontWeight: '500', textAlign: 'center', letterSpacing: 0.5}}>Accept Order</Text>
                 </View>
 
                 
---
+++ src/screens/OrderDelivery/styles.js
@@ -0,0 +1,56 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        backgroundColor: 'lightgrey', 
+        flex: 1
+    },
+    handleIndicator: {
+        backgroundColor: "black", 
+        width: 100
+    },
+    handleIndicatorContainer: { 
+        marginTop: 10, 
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'center'
+    },
+    routeDetailsText: {
+        fontSize: 25, 
+        letterSpacing: 1
+    },
+    orderDeilveryDetailsContainer: {
+        paddingHorizontal: 20,
+    },
+    restaurantName: {
+        fontSize: 25, 
+        letterSpacing: 1, 
+        paddingVertical: 20
+    },
+    addressContainer: {
+        flexDirection: 'row', 
+        marginBottom: 20, 
+        alignItems: 'center'
+    },
+    addressText: {
+        fontSize: 20, 
+        color: 'grey', 
+        fontWeight: '500', 
+        letterSpacing: 1, 
+        marginLeft: 15,
+    },
+    orderDetailsContainer: {
+        borderTopWidth: 1, 
+        borderColor: 'lightgrey', 
+        paddingTop: 20,
+    },
+    orderItemText: {
+        fontSize: 18, 
+        color: 'grey', 
+        fontWeight: '500', 
+        letterSpacing: 0.5, 
+        marginBottom: 5,
+    }
+
+
+});
